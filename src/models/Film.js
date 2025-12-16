d63f861f8a1d66e05275159a0a0925eadb8e4814
AnthonyLaurent1 <anthony061214@gmail.com>
2025-12-16 14:16:50 +0100

    jsonserver

---
+++ src/models/Film.js
@@ -0,0 +1,9 @@
+export default class Film {
+    constructor({name, image, plot, rate, comment}) {
+        this.name = name;
+        this.image = image;
+        this.plot = plot;
+        this.rate = rate;
+        this.comment = comment;
+    };
+}
